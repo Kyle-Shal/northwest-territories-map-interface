5194c00fec7ed3ded14238293c18f9c32a4ea40c
Kyle Shal <kashal@mun.ca>
2022-06-21 16:11:20 -0230

    Initial scaffold using vite

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+
+import Map from 'react-map-gl';
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Kyle Shal</h1>
+    </div>
+  )
+}
+
+export default App
