7d2c32ae8ffafc6907d49f45ab4eeb6ceb2ba584
Kyle Shal <kashal@mun.ca>
2022-07-04 12:28:21 -0230

    clean code

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -5,28 +5,6 @@ import * as turf from "@turf/turf";
 import nwtData from "../northwest_territories_rivers.json";
 // import diffData from "../difference.json";
 
-// import RasterLayer from './components/RasterLayer';
-
-const maskLayer = {
-  id: "zmask",
-  source: "mask",
-  type: "fill",
-  paint: {
-    "fill-color": "white",
-    "fill-opacity": 0.3,
-  },
-};
-
-const layerStyle = {
-  id: "point",
-  type: "fill",
-  paint: {
-    "fill-color": "transparent",
-    "fill-opacity": 1.0,
-    "fill-outline-color": "#fff",
-  },
-};
-
 const sensors = {
   "sentinel-1": {
     id: "sentinel-1",
@@ -39,6 +17,16 @@ const sensors = {
     label: "Sentinel 2",
     type: "wmts",
     url: `https://services.sentinel-hub.com/ogc/wms/318be53b-974b-4918-8f5f-53ba0a37079c?showLogo=false&service=WMTS&request=GetMap&layers=TRUE_COLOR&styles=&format=image%2Fpng&transparent=true&version=1.1.1&name=Sentinel-2&maxcc=60&height=512&width=512&srs=EPSG%3A3857&exceptions=BLANK&bbox={bbox-epsg-3857}&time=2019-09-01/2019-09-01`,
+  },
+};
+
+const layerStyle = {
+  id: "point",
+  type: "fill",
+  paint: {
+    "fill-color": "transparent",
+    "fill-opacity": 1.0,
+    "fill-outline-color": "#fff",
   },
 };
 
@@ -58,12 +46,7 @@ function SentinelSource({ sourceId, url }) {
 
   return (
     <Source key={sourceId} id={sourceId} type="raster" tiles={[url]}>
-      <Layer
-        key={layerId}
-        id={layerId}
-        beforeId={"building-outline"}
-        {...layer}
-      />
+      <Layer key={layerId} id={layerId} {...layer} />
     </Source>
   );
 }
@@ -79,7 +62,7 @@ function MapLayers({ value }) {
         zoom: 3.5,
       }}
       maxBounds={bbox}
-      mapStyle="mapbox://styles/mapbox/light-v10"
+      mapStyle="mapbox://styles/mapbox/satellite-v9"
       mapboxAccessToken="pk.eyJ1Ijoia3lsZS1zaGFsIiwiYSI6ImNsNG9qOWdlODA0MGMzY25vNHZ6M3IyOWUifQ.A3wEO8a8heSNUkhDKOeHYA"
     >
       {value === "sentinel-1" && (
