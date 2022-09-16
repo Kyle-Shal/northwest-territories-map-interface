525294eb94afc964dfc9dd18ddbe9b890ccae071
Kyle Shal <kashal@mun.ca>
2022-09-16 09:15:42 -0230

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,7 +17,9 @@ function Header({ value, onChange, opacityValue, onOpacityChange }) {
       color="white"
     >
       <Flex align="center" mr={5}>
-        <img src={logo} alt="logo" width={125}></img>
+        {
+          //<img src={logo} alt="logo" width={125}></img>
+        }
         <Stack direction="row" h="50px" p={4}>
           <Heading as="h1" size="md">
             NORTHWEST TERRITORIES | {value.toUpperCase()}
